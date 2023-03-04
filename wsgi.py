b5fa42c117352dbc34d4d483b1ea86b176a78532
Pulkit Madaan <pulkit16257@iiitd.ac.in>
2023-03-04 15:39:59 -0500

    add port customisation

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app.app import app, socketio
+import sys
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, port=sys.argv[1] if len(sys.argv) > 1 else None, debug=True)
